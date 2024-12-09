ceef317c6712705f9ce8439d631b15286e3a17c8
tylaar1 <151372152+tylaar1@users.noreply.github.com>
2024-12-09 12:44:05 +0000

    discreetized state space and created reward function
    Next step is to actually make the policy

--- custom_controller.py
+++ custom_controller.py
@@ -20,12 +20,24 @@ class CustomController(FlightController):
         ]
         self.q_table = {}
         self.state_space_size = 100
-        
-    def get_reward(self):
-       
-        
+    def discretize_state(self,x,y,target_x,target_y): #as far as i am aware need discreet space for q learning
+        distance_to_target = np.sqrt((x - target_x)**2 + (y - target_y)**2)
+
+        state= int(distance_to_target * 10)  #multiply by 10 means we essentially round to nearest 0.1 rathrer than nearest 1
+        state= min(state_bin, self.state_space_size - 1)  #make sure drone stays in screen 
+        #important for q learning as if space infinate drone can fly in wrong direction forever in name of exploration
+
+        return state    
+    def get_reward(self, distance_to_target):
+        if distance_to_target < 0.1:  #reward for hitting target
+            reward = 100
+        else:
+            reward = -distance_to_target #directly encourage movement towards target as target is known
+        return reward     
     def train(self):
         pass    
+    #epsilon greedy training policy to explore different routes
+    #targets appear in same locations each time so only have to learn the route rather than to fly specifically to target
     def get_thrusts(self, drone: Drone) -> Tuple[float, float]:
         return (0.5, 0.5) # Replace this with your custom algorithm
     def load(self):
